3b663a3e8e0f0f658b024f109e347f03ccaed816
Oleg1717 <olekmail@yandex.ru>
2023-03-08 18:10:08 +0300

    переделал метод добавления файла в аллюр отчет

--- src/test/java/globalsqa/com/helpers/AllureHelper.java
+++ src/test/java/globalsqa/com/helpers/AllureHelper.java
@@ -2,10 +2,14 @@ package globalsqa.com.helpers;
 
 import io.qameta.allure.Attachment;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class AllureHelper {
 
     @Attachment(value = "{attachName}", type = "application/csv", fileExtension = ".csv")
-    public static String attachAsCsvFile(String attachName, String data) {
-        return data;
+    public static byte[] attachCsvFile(String attachName, String filePath) throws IOException {
+        return Files.readAllBytes(Paths.get(filePath));
     }
 }
