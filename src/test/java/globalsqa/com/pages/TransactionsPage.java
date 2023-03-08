f10ee97ae6e877a4b6ed9b359e08303d204f07cc
Oleg1717 <olekmail@yandex.ru>
2023-03-08 18:17:03 +0300

    добавил ожидание загрузки данных в метод checkTransactionsAndAttachCsv

--- src/test/java/globalsqa/com/pages/TransactionsPage.java
+++ src/test/java/globalsqa/com/pages/TransactionsPage.java
@@ -1,11 +1,17 @@
 package globalsqa.com.pages;
 
+import globalsqa.com.config.ProjectConfig;
 import globalsqa.com.data.Transaction;
 import globalsqa.com.helpers.AllureHelper;
+import globalsqa.com.utils.FileUtils;
+import org.aeonbits.owner.ConfigFactory;
 import org.assertj.core.api.Assertions;
+import org.awaitility.Awaitility;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 import java.time.format.DateTimeFormatter;
 import java.util.List;
@@ -13,23 +19,30 @@ import java.util.stream.Collectors;
 
 import static io.qameta.allure.Allure.step;
 import static java.lang.String.format;
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.not;
 
 public class TransactionsPage {
 
     private final WebDriver driver;
+    private final ProjectConfig projectConfig = ConfigFactory.newInstance()
+            .create(ProjectConfig.class, System.getProperties());
 
     public TransactionsPage(WebDriver driver) {
         this.driver = driver;
+        PageFactory.initElements(driver, this);
     }
 
-    private final By tableTr = By.cssSelector("tr[id^=anchor]");
-    private final By tableTd = By.cssSelector("td.ng-binding");
+    @FindBy(css = "tr[id^=anchor]")
+    private List<WebElement> tableTr;
 
     private List<Transaction> getActualTransactionsData() {
-        return driver.findElements(tableTr)
+        driver.navigate().refresh();
+        return tableTr
                 .stream()
                 .map(tr -> {
-                    List<WebElement> tdList = tr.findElements(tableTd);
+                    List<WebElement> tdList = tr.findElements(By.cssSelector("td.ng-binding"));
                     return new Transaction(tdList.get(0).getText(),
                             tdList.get(1).getText(), tdList.get(2).getText());
                 })
@@ -38,7 +51,11 @@ public class TransactionsPage {
 
     public void checkTransactionsAndAttachCsv(List<Transaction> expectedTransactions) {
         step("Проверить транзакции и приложить отчет", () -> {
-            List<Transaction> actualTransactions = getActualTransactionsData();
+            List<Transaction> actualTransactions = Awaitility
+                    .with().pollInterval(2, SECONDS)
+                    .atMost(10, SECONDS)
+                    .given()
+                    .await().until(this::getActualTransactionsData, not(empty()));
             checkTransactionsCount(actualTransactions.size(), expectedTransactions.size());
             checkTransactions(actualTransactions, expectedTransactions);
             exportActualTransactionsToCsv(actualTransactions);
@@ -69,7 +86,8 @@ public class TransactionsPage {
                     .map(t -> String.join(";",
                             t.dateTime().format(expectedFormat), t.amount(), t.transactionType()))
                     .collect(Collectors.joining("\n"));
-            AllureHelper.attachAsCsvFile("Список транзакций", actualTransactionsDataInCsvFormat);
+            FileUtils.writeDataToFile(projectConfig.csvFilePath(), actualTransactionsDataInCsvFormat);
+            AllureHelper.attachCsvFile("Список транзакций", projectConfig.csvFilePath());
         });
     }
 }
